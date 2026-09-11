58a53b8fafe4555bfe5c63f8c842d57f1601ac4f
Moonats <natashanajoan@gmail.com>
2026-09-11 14:57:24 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,23 +1,14 @@
-const CACHE_NAME = 'todo-cache-v2';
-const urlsToCache = [
-  './',
-  './index.html',
-  './app.js',
-  './manifest.json'
-];
+const CACHE_NAME = 'todo-cache-v3'; // Naik ke versi 3
+const urlsToCache = ['./', './index.html', './app.js', './manifest.json'];
 
 self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(urlsToCache);
-    })
-  );
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
 });
-
+self.addEventListener('activate', event => {
+  event.waitUntil(caches.keys().then(keys => Promise.all(
+    keys.map(key => { if (key !== CACHE_NAME) return caches.delete(key); })
+  )));
+});
 self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
-  );
+  event.respondWith(caches.match(event.request).then(res => res || fetch(event.request)));
 });
