97c061d77ca86e80232046804513e26a4f947341
Moonats <natashanajoan@gmail.com>
2026-09-11 15:14:43 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'todo-cache-v3'; // Naik ke versi 3
+const CACHE_NAME = 'orbit-cache-v4'; // Versi 4
 const urlsToCache = ['./', './index.html', './app.js', './manifest.json'];
 
 self.addEventListener('install', event => {
